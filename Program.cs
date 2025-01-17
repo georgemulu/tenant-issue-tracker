4c7ef1128c72c26ef71f46722c6cdd761c306208
George Muthama Mulu <155823969+georgemulu@users.noreply.github.com>
2025-01-17 12:06:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
